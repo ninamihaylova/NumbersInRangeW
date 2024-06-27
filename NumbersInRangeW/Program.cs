10c6442c9026380652b6ebfacc6fba3d86752fe6
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-27 12:27:14 +0300

    Initial commit

---
+++ NumbersInRangeW/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+class NumbersInRangeW
+{
+   public static void Main(string[]args)
+   {
+       int number = int.Parse(Console.ReadLine());
+
+       while (number < 1 || number > 100)
+       {
+           number = int.Parse(Console.ReadLine());
+       }
+
+       Console.WriteLine(number);
+       }
+
+   } 
+    
